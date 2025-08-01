63651bd479aa94a15847377771af6ad7e054e3a5
kwkrass <karsten.wysk@gmail.com>
2025-08-01 11:05:10 +0200

    Fix environment variable replacement - use export const instead of import statements

--- ci/replace-envs.sh
+++ ci/replace-envs.sh
@@ -3,36 +3,40 @@ set -e
 
 echo "Starting environment variable replacement..."
 
-# Replace environment variables in firestore.js
-echo "const GOOGLE_FIREBASE_WEB_CLIENT_ID = \"$GOOGLE_FIREBASE_WEB_CLIENT_ID_PROD\"" > temp_firestore_envs.txt
-echo "const GOOGLE_FIREBASE_WEB_API_KEY = \"$GOOGLE_FIREBASE_WEB_API_KEY_PROD\"" >> temp_firestore_envs.txt
-echo "const GOOGLE_FIREBASE_WEB_AUTH_DOMAIN = \"$GOOGLE_FIREBASE_WEB_AUTH_DOMAIN_PROD\"" >> temp_firestore_envs.txt
-echo "const GOOGLE_FIREBASE_WEB_DATABASE_URL = \"$GOOGLE_FIREBASE_WEB_DATABASE_URL_PROD\"" >> temp_firestore_envs.txt
-echo "const GOOGLE_FIREBASE_WEB_PROJECT_ID = \"$GOOGLE_FIREBASE_WEB_PROJECT_ID_PROD\"" >> temp_firestore_envs.txt
-echo "const GOOGLE_FIREBASE_STORAGE_BUCKET = \"$GOOGLE_FIREBASE_STORAGE_BUCKET_PROD\"" >> temp_firestore_envs.txt
-echo "const GOOGLE_FIREBASE_WEB_MESSAGING_SENDER_ID = \"$GOOGLE_FIREBASE_WEB_MESSAGING_SENDER_ID_PROD\"" >> temp_firestore_envs.txt
-echo "const GOOGLE_FIREBASE_WEB_APP_ID = \"$GOOGLE_FIREBASE_WEB_APP_ID_PROD\"" >> temp_firestore_envs.txt
-echo "const SENTRY_DSN = \"$SENTRY_DSN\"" >> temp_firestore_envs.txt
-echo "const HOSTING_URL = \"$HOSTING_URL_PROD\"" >> temp_firestore_envs.txt
-echo "const CURRENT_ENVIORNMENT = \"$CURRENT_ENVIORNMENT_PROD\"" >> temp_firestore_envs.txt
-echo "const NOTES_COLLABORATION_SERVER = \"$NOTES_COLLABORATION_SERVER\"" >> temp_firestore_envs.txt
-echo "const ALGOLIA_APP_ID = \"$ALGOLIA_APP_ID_PROD\"" >> temp_firestore_envs.txt
-echo "const ALGOLIA_SEARCH_ONLY_API_KEY = \"$ALGOLIA_SEARCH_ONLY_API_KEY_PROD\"" >> temp_firestore_envs.txt
-echo "const GOOGLE_FIREBASE_WEB_NOTES_STORAGE_BUCKET = \"$GOOGLE_FIREBASE_WEB_NOTES_STORAGE_BUCKET_PROD\"" >> temp_firestore_envs.txt
-echo "const IP_REGISTRY_API_KEY = \"$IP_REGISTRY_API_KEY\"" >> temp_firestore_envs.txt
-echo "const SIB_API_KEY = \"$SIB_API_KEY\"" >> temp_firestore_envs.txt
-echo "const SIB_MARKETING_SERVICE_LIST = \"$SIB_MARKETING_SERVICE_LIST_PROD\"" >> temp_firestore_envs.txt
-echo "const GOOGLE_ANALYTICS_KEY = \"$GOOGLE_ANALYTICS_KEY_PROD\"" >> temp_firestore_envs.txt
-echo "const GOOGLE_ADS_GUIDE_CONVERSION_TAG = \"$GOOGLE_ADS_GUIDE_CONVERSION_TAG_PROD\"" >> temp_firestore_envs.txt
-echo "const GIPHY_API_KEY = \"$GIPHY_API_KEY_PROD\"" >> temp_firestore_envs.txt
-echo "const PERPLEXITY_API_KEY = \"$PERPLEXITY_API_KEY_PROD\"" >> temp_firestore_envs.txt
+# Replace environment variables in firestore.js (maintain import syntax)
+cat > temp_firestore_envs.txt << EOF
+export const GOOGLE_FIREBASE_WEB_CLIENT_ID = "$GOOGLE_FIREBASE_WEB_CLIENT_ID_PROD"
+export const GOOGLE_FIREBASE_WEB_API_KEY = "$GOOGLE_FIREBASE_WEB_API_KEY_PROD"
+export const GOOGLE_FIREBASE_WEB_AUTH_DOMAIN = "$GOOGLE_FIREBASE_WEB_AUTH_DOMAIN_PROD"
+export const GOOGLE_FIREBASE_WEB_DATABASE_URL = "$GOOGLE_FIREBASE_WEB_DATABASE_URL_PROD"
+export const GOOGLE_FIREBASE_WEB_PROJECT_ID = "$GOOGLE_FIREBASE_WEB_PROJECT_ID_PROD"
+export const GOOGLE_FIREBASE_STORAGE_BUCKET = "$GOOGLE_FIREBASE_STORAGE_BUCKET_PROD"
+export const GOOGLE_FIREBASE_WEB_MESSAGING_SENDER_ID = "$GOOGLE_FIREBASE_WEB_MESSAGING_SENDER_ID_PROD"
+export const GOOGLE_FIREBASE_WEB_APP_ID = "$GOOGLE_FIREBASE_WEB_APP_ID_PROD"
+export const SENTRY_DSN = "$SENTRY_DSN"
+export const HOSTING_URL = "$HOSTING_URL_PROD"
+export const CURRENT_ENVIORNMENT = "$CURRENT_ENVIORNMENT_PROD"
+export const NOTES_COLLABORATION_SERVER = "$NOTES_COLLABORATION_SERVER"
+export const ALGOLIA_APP_ID = "$ALGOLIA_APP_ID_PROD"
+export const ALGOLIA_SEARCH_ONLY_API_KEY = "$ALGOLIA_SEARCH_ONLY_API_KEY_PROD"
+export const GOOGLE_FIREBASE_WEB_NOTES_STORAGE_BUCKET = "$GOOGLE_FIREBASE_WEB_NOTES_STORAGE_BUCKET_PROD"
+export const IP_REGISTRY_API_KEY = "$IP_REGISTRY_API_KEY"
+export const SIB_API_KEY = "$SIB_API_KEY"
+export const SIB_MARKETING_SERVICE_LIST = "$SIB_MARKETING_SERVICE_LIST_PROD"
+export const GOOGLE_ANALYTICS_KEY = "$GOOGLE_ANALYTICS_KEY_PROD"
+export const GOOGLE_ADS_GUIDE_CONVERSION_TAG = "$GOOGLE_ADS_GUIDE_CONVERSION_TAG_PROD"
+export const GIPHY_API_KEY = "$GIPHY_API_KEY_PROD"
+export const PERPLEXITY_API_KEY = "$PERPLEXITY_API_KEY_PROD"
+EOF
 
 sed -i '/BEGIN-ENVS/,/END-ENVS/d' utils/backends/firestore.js
 sed -i '/\/\/ BEGIN-ENVS/r temp_firestore_envs.txt' utils/backends/firestore.js
 
-# Replace environment variables in apisConfig.js
-echo "const GOOGLE_FIREBASE_WEB_CLIENT_ID = \"$GOOGLE_FIREBASE_WEB_CLIENT_ID_PROD\"" > temp_apis_envs.txt
-echo "const GOOGLE_FIREBASE_WEB_API_KEY = \"$GOOGLE_FIREBASE_WEB_API_KEY_PROD\"" >> temp_apis_envs.txt
+# Replace environment variables in apisConfig.js (maintain import syntax)
+cat > temp_apis_envs.txt << EOF
+export const GOOGLE_FIREBASE_WEB_CLIENT_ID = "$GOOGLE_FIREBASE_WEB_CLIENT_ID_PROD"
+export const GOOGLE_FIREBASE_WEB_API_KEY = "$GOOGLE_FIREBASE_WEB_API_KEY_PROD"
+EOF
 
 sed -i '/BEGIN-ENVS/,/END-ENVS/d' apis/google/apisConfig.js
 sed -i '/\/\/ BEGIN-ENVS/r temp_apis_envs.txt' apis/google/apisConfig.js
