76e155920f40c68d308f94dfbcfa2f56b7c66fb3
kwkrass <karsten.wysk@gmail.com>
2025-08-01 11:09:50 +0200

    Preserve original commit dates and committer info when pushing to alldoneapp_github

--- ci/github-push.sh
+++ ci/github-push.sh
@@ -44,10 +44,13 @@ if [ -n "$CUTOFF_COMMIT" ]; then
     AUTHOR_NAME=$(git show -s --format="%an" $commit)
     AUTHOR_EMAIL=$(git show -s --format="%ae" $commit)
     AUTHOR_DATE=$(git show -s --format="%ad" $commit)
+    COMMITTER_NAME=$(git show -s --format="%cn" $commit)
+    COMMITTER_EMAIL=$(git show -s --format="%ce" $commit)
+    COMMITTER_DATE=$(git show -s --format="%cd" $commit)
     COMMIT_MESSAGE=$(git show -s --format="%B" $commit | tr -d "\r")
     git checkout $commit -- . || true
     git add -A
-    GIT_AUTHOR_NAME="$AUTHOR_NAME" GIT_AUTHOR_EMAIL="$AUTHOR_EMAIL" GIT_AUTHOR_DATE="$AUTHOR_DATE" git commit -m "$COMMIT_MESSAGE" --allow-empty || true
+    GIT_AUTHOR_NAME="$AUTHOR_NAME" GIT_AUTHOR_EMAIL="$AUTHOR_EMAIL" GIT_AUTHOR_DATE="$AUTHOR_DATE" GIT_COMMITTER_NAME="$COMMITTER_NAME" GIT_COMMITTER_EMAIL="$COMMITTER_EMAIL" GIT_COMMITTER_DATE="$COMMITTER_DATE" git commit -m "$COMMIT_MESSAGE" --allow-empty || true
   done
 else
   echo "No cutoff commit found, using all history"
