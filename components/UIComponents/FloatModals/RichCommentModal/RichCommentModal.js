fb0ae2d1f9dabb61f44e30a46f41a28f10993c3b
Karsten Wysk <karsten.wysk@gmail.com>
2026-07-27 08:59:59 +0000

    Merge branch 'ai/at-2059-lightweight-feature-pipelines' into 'master'
    Limit feature branch pipeline work
    
    See merge request alldonegmbh/alldone!184

--- components/UIComponents/FloatModals/RichCommentModal/RichCommentModal.js
+++ components/UIComponents/FloatModals/RichCommentModal/RichCommentModal.js
@@ -120,7 +120,7 @@ export default function RichCommentModal({
         projectId,
         objectId,
         objectType === 'users' ? 'contacts' : objectType,
-        3
+        2
     )
 
     disableDoneButton = !!botOptionModalIsOpen
