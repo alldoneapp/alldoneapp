f9ebc2ab52142623e42ab7a35f986da2a714a844
Karsten Wysk <karsten.wysk@gmail.com>
2026-07-27 09:00:45 +0000

    Merge branch 'ai/at-2060-show-three-comments' into 'master'
    Show last three comments in comment popup
    
    See merge request alldonegmbh/alldone!185

--- components/UIComponents/FloatModals/RichCommentModal/RichCommentModal.js
+++ components/UIComponents/FloatModals/RichCommentModal/RichCommentModal.js
@@ -120,7 +120,7 @@ export default function RichCommentModal({
         projectId,
         objectId,
         objectType === 'users' ? 'contacts' : objectType,
-        2
+        3
     )
 
     disableDoneButton = !!botOptionModalIsOpen
